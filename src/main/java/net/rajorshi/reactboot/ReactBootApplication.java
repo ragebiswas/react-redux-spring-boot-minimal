ed7966ebf251b366c408756a7076d23096854bd0
Rajorshi Biswas <ragebiswas@gmail.com>
2016-07-24 18:44:27 +0530

    Added base spring-boot project

---
+++ src/main/java/net/rajorshi/reactboot/ReactBootApplication.java
@@ -0,0 +1,12 @@
+package net.rajorshi.reactboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBootApplication.class, args);
+	}
+}
---
+++ src/test/java/net/rajorshi/reactboot/ReactBootApplicationTests.java
@@ -0,0 +1,18 @@
+package net.rajorshi.reactboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ReactBootApplication.class)
+@WebAppConfiguration
+public class ReactBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
